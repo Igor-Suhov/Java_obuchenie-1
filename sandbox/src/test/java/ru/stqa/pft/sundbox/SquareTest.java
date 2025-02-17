33680fcf26184cbb4895bd5b4dbc8c8b2a2b09ed
Igor-Suhov <igorsuhov1@mail.ru>
2025-02-17 14:12:59 +0300

    Update SquareTest.java

--- sandbox/src/test/java/ru/stqa/pft/sundbox/SquareTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sundbox/SquareTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.sundbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.sandbox.Square;
 
@@ -7,6 +8,6 @@ public class SquareTest {
   @Test
   public void testArea(){
     Square s = new Square(5);
-    assert s.area() == 25;
+    Assert.assertEquals( s.area(), 25.0);
   }
 }
