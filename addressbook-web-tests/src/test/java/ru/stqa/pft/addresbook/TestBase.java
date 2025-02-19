e38d70606a361dc60f2764e5fb1e68698f9493fe
Igor-Suhov <igorsuhov1@mail.ru>
2025-02-19 09:31:44 +0300

    Create TestBase.java

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/TestBase.java
@@ -0,0 +1,220 @@
+package ru.stqa.pft.addresbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class TestBase {
+  private WebDriver driver;
+
+  @BeforeClass
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+   /// System.setProperty("webdriver.chrome.driver", "C:/path/to/chromedriver.exe"); // Убедитесь, что путь к chromedriver указан
+    driver = new ChromeDriver();
+    login();
+  }
+
+  private void login() {
+    driver.get("http://localhost/addressbook/group.php");
+    driver.findElement(By.id("LoginForm")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.id("LoginForm")).submit();
+  }
+
+  protected void returnToGroupPage() {
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  protected void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  // jj
+  protected void fillGroupForm(GroupData groupData) {
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  protected void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  protected void gotoGroupPage() {
+    driver.findElement(By.linkText("groups")).click();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    driver.quit();
+   }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+
+  protected void deleteSelectGroups() {
+    driver.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+  }
+
+  protected void selestGroup() {
+    driver.findElement(By.name("selected[]")).click();
+  }
+}
+
+
+/*package ru.stqa.pft.addresbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+import static org.testng.Assert.fail;
+
+public class TestBase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  private JavascriptExecutor js;
+
+  @BeforeClass
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    js = (JavascriptExecutor) driver;
+    login();
+  }
+
+  private void login() {
+    driver.get("http://localhost/addressbook/group.php");
+    driver.findElement(By.id("LoginForm")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("username");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.id("LoginForm")).submit();
+  }
+
+  protected void returnToGroupPage() {
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  protected void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  //jj
+  protected void fillGroupForm(GroupData groupData) {
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    driver.findElement(By.name("group_parent_id")).click();
+    driver.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  protected void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  protected void gotoGroupPage() {
+    driver.findElement(By.linkText("groups")).click();
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+
+  protected void deleteSelectGroups() {
+    driver.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+  }
+
+  protected void selestGroup() {
+    driver.findElement(By.xpath("//div[@id='content']/form/span[2]/input")).click();
+  }
+}*/
