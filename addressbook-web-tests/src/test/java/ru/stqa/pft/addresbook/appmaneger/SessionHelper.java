0606f1c5639183c1079c97c1f27782a5669d12b1
Igor-Suhov <igorsuhov1@mail.ru>
2025-02-19 13:09:58 +0300

    Update SessionHelper.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmaneger/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmaneger/SessionHelper.java
@@ -3,21 +3,17 @@ package ru.stqa.pft.addresbook.appmaneger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-  private final WebDriver driver;
+public class SessionHelper extends HelperBase{
+
 
   public SessionHelper(WebDriver driver) {
 
-    this.driver = driver;
+    super(driver);
   }
   public void login() {
     driver.get("http://localhost/addressbook/group.php");
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.id("LoginForm")).submit();
+    type (By.name("user"),"admin");
+    type(By.name("pass"),"secret");
+    click(By.id("LoginForm"));
   }
 }
