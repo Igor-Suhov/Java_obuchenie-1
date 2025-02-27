bd4385c3bc6e111a4f7e300ce2e26e8a7e8f156d
Igor-Suhov <igorsuhov1@mail.ru>
2025-02-27 14:13:57 +0300

    Create MyFirstProgram.java

---
+++ sandbox/src/main/java/ru/stqa/pft/sundbox/MyFirstProgram.java
@@ -0,0 +1,32 @@
+
+
+package ru.stqa.pft.sundbox;
+
+import ru.stqa.pft.sundbox.Rectangle;
+import ru.stqa.pft.sundbox.Square;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+
+
+        hello("World");
+        hello("User");
+        hello("Igor");
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной" + s.l +  "=" + s.area());
+        Rectangle r= new Rectangle(4,6);
+
+          System.out.println("Площадь прямоугольника со сторонами " +r.a +  "и" +r.b+ "=" + r.area());
+    }
+    public static void hello (String somebody ) {
+
+        System.out.println ("Hello, " + somebody + "!");
+    }
+    public static double   area (double len) {
+        return len * len;
+    }
+    public static double area (double a, double b){
+        return a*b;
+    }
+}
