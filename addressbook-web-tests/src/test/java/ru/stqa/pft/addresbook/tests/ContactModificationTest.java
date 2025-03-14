490435d7f19833c9c82135cd32afde3f09df739d
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:47 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactModificationTest.java
@@ -1,13 +1,22 @@
 package ru.stqa.pft.addresbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.ContactData;
 
 public class ContactModificationTest extends TestBase {
   @Test
   public void testContactModification (){
-    app.getContactHelper().editClickContact();
-    app.getContactHelper().editBook(new ContactData("igor", "Suhov", "Voronech", "89003078510",null) , true);
+    int before = app.getContactHelper().getContactCount();
+    System.out.println("Количество контактов до обновления: " + before);
+    app.getContactHelper().selectedContact();
+
+    app.getContactHelper().editContact();
+    app.getContactHelper().editBook(new ContactData("igor1", "Suhov1", "Voronech1", "890030785101",null) , true);
     app.getContactHelper().saveDate();
+    int after = app.getContactHelper().getContactCount();
+    System.out.println("Количество контактов после обновления : " + after);
+
+    Assert.assertEquals(after, before );
   }
 }
