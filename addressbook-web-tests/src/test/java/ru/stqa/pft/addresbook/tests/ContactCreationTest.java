5f12814933af540fcf44862fd265aad0c316edfa
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:40 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactCreationTest.java
@@ -1,20 +1,40 @@
 package ru.stqa.pft.addresbook.tests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.ContactData;
 
 
 public class ContactCreationTest extends TestBase{
-
-
   @Test
+  public void testContactCreation() {
+    int before = app.getContactHelper().getContactCount();
+    System.out.println("Количество контактов до добавления: " + before);
+
+    app.getContactHelper().gotoNewContact();
+    app.getContactHelper().editBook(new ContactData("igor", "Suhov", "Voronech", "89003078510", "test2"), true);
+    app.getContactHelper().saveDate();
+
+    int after = app.getContactHelper().getContactCount();
+    System.out.println("Количество контактов после добавления: " + after);
+
+    Assert.assertEquals(after, before + 1);
+  }
+}
+
+
+  /*@Test
   public void testContactCreation () {
+    int before =app.getContactHelper().getContactCount();
     app.getContactHelper().gotoNewContact();
     app.getContactHelper().editBook(new ContactData("igor", "Suhov", "Voronech", "89003078510","test2"),true);
     app.getContactHelper().saveDate();
+    int after =app.getContactHelper().getContactCount();
+    Assert.assertEquals(after,before+1);
   }
 
 
-}
+}*/
+
 
 
 
