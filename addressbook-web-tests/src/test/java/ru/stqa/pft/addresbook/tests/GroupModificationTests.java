2edf1dfa7d1452348be18c734f50ff1ce2d37e0e
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:59 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
@@ -1,16 +1,27 @@
 package ru.stqa.pft.addresbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupModificationTests extends TestBase{
   @Test
   public void testGroupModification(){
+
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selestGroup();
+    List<GroupData> before = app.getGroupHelper().getGrouplist();
+     app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroup(before.size() -1 );
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
-    app.getGroupHelper().submitGroupModyfication();
+    app.getGroupHelper().fillGroupForm(new GroupData("test2", null, null));
+    app.getGroupHelper().submitGroupModification();
+
     app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGrouplist();
+     app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size());
+
   }
 }
