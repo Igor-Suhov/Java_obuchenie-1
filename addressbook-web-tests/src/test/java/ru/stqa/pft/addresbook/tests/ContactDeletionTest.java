4ae0d0bae3d2541cf34e8124ce7277fd81d26a5c
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:44 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTest.java
@@ -1,11 +1,18 @@
 package ru.stqa.pft.addresbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addresbook.model.ContactData;
 
 public class ContactDeletionTest extends TestBase{
   @Test
   public void testContactDeletion(){
+    int before = app.getContactHelper().getContactCount();
+    System.out.println("Количество контактов до добавления: " + before);
     app.getContactHelper().deletionContact();
+    int after = app.getContactHelper().getContactCount();
+    System.out.println("Количество контактов после добавления: " + after);
+
+    Assert.assertEquals(after, before - 1);
+
   }
 }
