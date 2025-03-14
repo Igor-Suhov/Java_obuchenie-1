bc5ba20d681ffc71bba8fc9abcff819443420a9b
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:54 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupDeletionTest.java
@@ -1,16 +1,31 @@
 
 package ru.stqa.pft.addresbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addresbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupDeletionTest extends TestBase {
 
   @Test
   public void testGroupDeletion() {
+
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selestGroup();
+    List<GroupData> before = app.getGroupHelper().getGrouplist();
+     app.getGroupHelper().getGroupCount();
+    System.out.println("Количество груп до добавления: " + before);
+
+    app.getGroupHelper().selectGroup(before.size() -1);
     app.getGroupHelper().deleteSelectGroups();
+
     app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGrouplist();
+     app.getGroupHelper().getGroupCount();
+    System.out.println("Количество груп после добавления: " + after);
+    Assert.assertEquals(after.size(), before.size() -1);
+
   }
 }
 
