c5fe237760c22b417dc8a642973946eb521c2e8b
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:51 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupCreationTest.java
@@ -2,18 +2,35 @@
 
 package ru.stqa.pft.addresbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.GroupData;
+
+import java.util.List;
 
 public class GroupCreationTest extends TestBase {
 
   @Test
   public void testGroupCreation() {
+
     app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGrouplist();
+     app.getGroupHelper().getGroupCount();
+   /// System.out.println("Количество груп до добавления: " + before);
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test2", "test3", "test4"));
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
+
+    List<GroupData> after = app.getGroupHelper().getGrouplist();
+   Assert.assertEquals(after.size(), before.size () +1);
+   app.getGroupHelper().getGroupCount();
+    //System.out.println("Количество груп после добавления: " + after);
+    //Assert.assertEquals(after, before +1);
+
+
+
+
   }
 }
 
