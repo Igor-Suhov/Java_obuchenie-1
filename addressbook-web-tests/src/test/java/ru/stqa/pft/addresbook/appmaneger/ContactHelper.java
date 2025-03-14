54794bd9df281b43968e6b92c87db91f60fd6a4e
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-14 10:59:25 +0300

    модуль 4 задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmaneger/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/appmaneger/ContactHelper.java
@@ -14,6 +14,9 @@ public class ContactHelper  extends HelperBase{
 
   public void saveDate() {
     By.xpath("//div[@id='content']/form/input[21]").findElement(driver).click();
+    //By.linkText("group page").findElement(driver).click();
+   // driver.findElement(By.linkText("home page")).click();
+    driver.get("http://localhost/addressbook/index.php");
   }
 
   public void editBook(ContactData contactData,boolean creation) {
@@ -29,10 +32,25 @@ public class ContactHelper  extends HelperBase{
   }
 
   public void deletionContact() {
+    /*driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("selected[]")).click();
+    driver.findElement(By.xpath("//input[@value='Delete']")).click();*/
+
     driver.get("http://localhost/addressbook/");
-    driver.findElement(By.id("11")).click();
+    driver.findElement(By.name("selected[]")).click();
     driver.findElement(By.xpath("//input[@value='Delete']")).click();
+   driver.switchTo().alert().accept();
+    driver.findElement(By.linkText("home")).click();
+  }
 
 
+
+  public int getContactCount() {
+    return driver.findElements(By.name("selected[]")).size();
+
+  }
+
+  public void editContact() {
+    driver.findElement(By.xpath("//table[@id='maintable']/tbody/tr[3]/td[8]/a/img")).click();
   }
 }
