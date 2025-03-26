e6643fafd5248f0c142664d43556a9e269f4e06b
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-26 15:27:44 +0300

    Update GroupModificationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -11,17 +12,25 @@ public class GroupModificationTests extends TestBase{
   public void testGroupModification(){
 
     app.getNavigationHelper().gotoGroupPage();
+    /*if (!app.getGroupHelper().isThereAgroup()){
+      app.getGroupHelper().createGroup(new GroupData("test2", null, null));
+    }*/
     List<GroupData> before = app.getGroupHelper().getGrouplist();
-     app.getGroupHelper().getGroupCount();
+     //app.getGroupHelper().getGroupCount();
+
     app.getGroupHelper().selectGroup(before.size() -1 );
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test2", null, null));
+    GroupData group = new GroupData(before.get(before.size() -1 ).getId(),"test1", "test2","test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
-
     app.getGroupHelper().returnToGroupPage();
+   //app.getGroupHelper().getGroupCount();
+
     List<GroupData> after = app.getGroupHelper().getGrouplist();
-     app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), before.size());
+    before.remove(before.size() -1);
+    before.add (group);
+    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 
   }
 }
