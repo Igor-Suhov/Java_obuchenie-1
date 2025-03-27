8bc56c87db4935edc803469647089ecb40b862b6
Igor-Suhov <igorsuhov1@mail.ru>
2025-03-27 12:43:37 +0300

    Update GroupModificationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupModificationTests.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -30,7 +30,10 @@ public class GroupModificationTests extends TestBase{
     Assert.assertEquals(after.size(), before.size());
     before.remove(before.size() -1);
     before.add (group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Comparator<? super GroupData> byid=(g1,g2) -> Integer.compare(g1.getId(), g2.getId()) ;
+    before.sort(byid);
+    after.sort(byid);
+    Assert.assertEquals(before,after);
 
   }
 }
