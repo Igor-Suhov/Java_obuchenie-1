c6b00ff176060d32317c122d277e42ad2d93f74e
Igor-Suhov <igorsuhov1@mail.ru>
2025-04-14 15:00:16 +0300

    Update ContactModificationTest.java
    Окончил  4 модуль

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactModificationTest.java
@@ -4,19 +4,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addresbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class ContactModificationTest extends TestBase {
+
+
   @Test
   public void testContactModification (){
-    int before = app.getContactHelper().getContactCount();
-    System.out.println("Количество контактов до обновления: " + before);
-    app.getContactHelper().selectedContact();
-
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectedContact(before.size()-1);
     app.getContactHelper().editContact();
-    app.getContactHelper().editBook(new ContactData("igor1", "Suhov1", "Voronech1", "890030785101",null) , true);
+    ContactData contact =new ContactData("Igor1", "Suhov1", "Voronech1", "89001", null);
+    app.getContactHelper().editBook(contact);
     app.getContactHelper().saveDate();
-    int after = app.getContactHelper().getContactCount();
-    System.out.println("Количество контактов после обновления : " + after);
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size());
 
-    Assert.assertEquals(after, before );
+    before.remove(before.size() -1);
+    before.add (contact);
+    Comparator<? super ContactData> byid=(g1, g2) -> Integer.compare(g1.getId(), g2.getId()) ;
+    before.sort (byid);
+    after.sort(byid);
+    Assert.assertEquals(before,after);
+
   }
+
 }
