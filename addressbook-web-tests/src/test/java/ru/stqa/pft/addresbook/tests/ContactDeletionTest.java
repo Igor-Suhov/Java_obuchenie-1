0f63a01c554760a23175175f1e7e1774d88c5a77
Igor-Suhov <igorsuhov1@mail.ru>
2025-04-14 15:00:13 +0300

    Update ContactDeletionTest.java
    Окончил  4 модуль

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/ContactDeletionTest.java
@@ -2,17 +2,23 @@ package ru.stqa.pft.addresbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addresbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactDeletionTest extends TestBase{
   @Test
   public void testContactDeletion(){
-    int before = app.getContactHelper().getContactCount();
-    System.out.println("Количество контактов до добавления: " + before);
-    app.getContactHelper().deletionContact();
-    int after = app.getContactHelper().getContactCount();
-    System.out.println("Количество контактов после добавления: " + after);
+    List<ContactData> before = app.getContactHelper().getContactList();
+    app.getContactHelper().deletionContact(before.size()-1);
+    List<ContactData> after = app.getContactHelper().getContactList();
 
-    Assert.assertEquals(after, before - 1);
+
+    Assert.assertEquals(after.size(), before.size()-1);
+
+    before.remove(before.size()-1);
+      Assert.assertEquals(before,after);
+    }
 
   }
-}
+
