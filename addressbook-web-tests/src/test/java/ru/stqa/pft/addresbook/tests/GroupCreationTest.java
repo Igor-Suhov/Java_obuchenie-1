3719f3ba5d61d0ad3e28b868c206a9e1c79f8a4d
Igor-Suhov <igorsuhov1@mail.ru>
2025-04-22 14:23:49 +0300

    Update GroupCreationTest.java
    Рефакторинг и оптимизация кода урок №5/4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/tests/GroupCreationTest.java
@@ -14,33 +14,22 @@ public class GroupCreationTest extends TestBase {
   @Test
   public void testGroupCreation() {
 
-    app.getNavigationHelper().gotoGroupPage();
-    List<GroupData> before = app.getGroupHelper().getGrouplist();
-    //app.getGroupHelper().getGroupCount();
-   ///System.out.println("Количество груп до добавления: " + before);
-    GroupData group =new GroupData("test1", null,null);
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
-    ///app.getGroupHelper().getGroupCount();
-    List<GroupData> after = app.getGroupHelper().getGrouplist();
+    app.goTo().groupPage();
+    List<GroupData> before = app.group().List();
+    GroupData group =new GroupData().withName("test1");
+    app.group().create(group);
+    List<GroupData> after = app.group().List();
    Assert.assertEquals(after.size(), before.size () +1);
+   //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 
-
-   group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
    before.add(group);
     Comparator<? super GroupData> byid=(g1, g2) -> Integer.compare(g1.getId(), g2.getId()) ;
    before.sort(byid);
    after.sort(byid);
    Assert.assertEquals(before,after);
-   //app.getGroupHelper().getGroupCount();
-    //System.out.println("Количество груп после добавления: " + after);
-    //Assert.assertEquals(after, before +1);
-
-
-///
 
   }
+
+
 }
 
