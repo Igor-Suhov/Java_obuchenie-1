9049339d3605a13bba99af0b9c220a68bdee4f06
Igor-Suhov <igorsuhov1@mail.ru>
2025-04-22 14:23:46 +0300

    Update GroupData.java
    Рефакторинг и оптимизация кода урок №5/4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addresbook/model/GroupData.java
@@ -3,30 +3,12 @@ package ru.stqa.pft.addresbook.model;
 import java.util.Objects;
 
 public class GroupData {
-
-  public  int id;
-  public final String name;
-  public final String header;
-
-
+  public  int id = Integer.MAX_VALUE;;
+  public  String name;
+  public  String header;
+  public  String footer;
 
 
-  public final String footer;
-
-
-
-  public GroupData (int id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-  public GroupData ( String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
 
 
   @Override
@@ -35,10 +17,6 @@ public class GroupData {
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
-  }
-
-  public void setId(int id) {
-    this.id = id;
   }
 
   @Override
@@ -57,16 +35,32 @@ public class GroupData {
   public int getId() {
     return id;
   }
-
   public String getName() {
     return name;
   }
-
   public String getHeader() {
     return header;
   }
-
   public String getFooter() {
     return footer;
   }
+  public GroupData  withId(int id) {
+    this.id = id;
+    return this;
+  }
+  public GroupData withName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public GroupData withHeader(String header) {
+    this.header = header;
+    return this;
+  }
+
+  public GroupData withFooter(String footer) {
+    this.footer = footer;
+    return this;
+  }
+
 }
